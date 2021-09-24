84e5cbd79d169e735ef2c5e00fa8af69b898761e
arnoldgq612 <arnoldgq612@gmail.com>
2021-09-23 18:35:38 -0600

    6.35pm

--- ANNEXES/DB_TEST_SCRIPTS/test_data_script.sql
+++ ANNEXES/DB_TEST_SCRIPTS/test_data_script.sql
@@ -10,3 +10,6 @@ INSERT INTO `si_db`.`si_roles` (`PK_ROL`, `description`) VALUES ('3', 'USER');
 INSERT INTO `si_db`.`si_officials` (`PK_OFFICIAL`, `name`, `surname`, `email`, `FK_department`) VALUES ('50', 'JOSEPH', 'GRANDA', 'informatica@sanpablo.go.cr', '103');
 INSERT INTO `si_db`.`si_officials` (`PK_OFFICIAL`, `name`, `surname`, `email`, `FK_department`) VALUES ('51', 'ISMAEL', 'SALAZAR', 'controlinterno@sanpablo.go.cr', '100');
 
+-- SELECT HEX(AES_ENCRYPT('Informacion', 'passwordSegura'));
+-- SELECT AES_DECRYPT(UNHEX('B18464A9F19465BFA2E59F2476E7C095'),'passwordSegura');
+-- SELECT cast(AES_DECRYPT(UNHEX('B18464A9F19465BFA2E59F2476E7C095'),'passwordSegura') AS char);
